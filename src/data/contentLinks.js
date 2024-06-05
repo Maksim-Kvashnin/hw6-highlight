ed9786788a463a4907f5b63ba7de7838c834f322
Maksim_KA <Kvashnin@hotmail.com>
2024-06-05 23:09:18 +0700

    done

---
+++ src/data/contentLinks.js
@@ -0,0 +1,43 @@
+const contentLinks = [
+  {
+    id: 'mzdzMJthhVOJWVgAWsBOV',
+    type: 'video',
+    url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+    title: 'Самые красивые цвета природы в 4K 🌻🐦Птицы и цветы.',
+    views: 50,
+  },
+  {
+    id: '6qaRTOdEZbzYJfTMJm6DM',
+    type: 'video',
+    url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+    title: 'лучший расслабляющий аквариум в 4K UHD 🐠',
+    views: 12,
+  },
+  {
+    id: 'QXxWn9sZvGkX7gYSwSbg4',
+    type: 'article',
+    title: 'Невероятные события в неизвестном поселке...',
+    views: 175,
+  },
+  {
+    id: '6qaRTOdEZbzYJfTMJm6DM',
+    type: 'article',
+    title: 'Секретные данные были раскрыты!',
+    views: 1532,
+  },
+  {
+    id: 'G5RdV1pOqHALXLT8hY1Oa',
+    type: 'video',
+    url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+    title: '4K Ultra HD Видео о диких животных',
+    views: 4253,
+  },
+  {
+    id: 'SMDTTcsKWo6P0uyxghX9T',
+    type: 'article',
+    title: 'Кот Бегемот обладает невероятной...',
+    views: 12,
+  },
+]
+
+export default contentLinks;
